7f5ede3e468c74a767c66b639c807144adc3d332
BuildTools <87946725+ORD1STAR@users.noreply.github.com>
2023-05-13 11:02:08 +0100

    r changes

--- index.js
+++ index.js
@@ -218,6 +218,15 @@ app.get("/password", (req, res) => {
 
     
 })
+app.get("/pagePersonelle", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/pagePerso.html'));
+})
+app.get("/Admin_Doleances", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/Admin_Doleances.html'));
+})
+app.get("/listeEtudiants", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/html/listeEtudiants.html'));
+})
 app.get("/disconnect", (req, res) => {
     res.clearCookie("token");
     res.redirect("/");
